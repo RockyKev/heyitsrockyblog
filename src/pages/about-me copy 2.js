cffec0672cf1353da1ed51c1aa2d83fb614e4761
Rocky Kev <kev.rocky@gmail.com>
2019-11-20 22:46:23 -0800

    updated front page

---
+++ src/pages/about-me copy 2.js
@@ -0,0 +1,89 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+class AboutMe extends React.Component {
+  // render() {
+  //   const { data } = this.props
+  //   const siteTitle = data.site.siteMetadata.title
+  //   const posts = data.allMdx.edges
+
+  //   return (
+  //     <Layout location={this.props.location} title={siteTitle}>
+  //       <SEO title="All posts" />
+  //       <Bio />
+  //       <div style={{ margin: "20px 0 40px" }}>
+  //         {posts.map(({ node }) => {
+  //           const title = node.frontmatter.title || node.fields.slug
+  //           return (
+  //             <div key={node.fields.slug}>
+  //               <h3
+  //                 style={{
+  //                   marginBottom: rhythm(1 / 4),
+  //                 }}
+  //               >
+  //                 <Link
+  //                   style={{ boxShadow: `none` }}
+  //                   to={`blog${node.fields.slug}`}
+  //                 >
+  //                   {title}
+  //                 </Link>
+  //               </h3>
+  //               <small>{node.frontmatter.date}</small>
+  //               <p
+  //                 dangerouslySetInnerHTML={{
+  //                   __html: node.frontmatter.description || node.excerpt,
+  //                 }}
+  //               />
+  //             </div>
+  //           )
+  //         })}
+  //       </div>
+  //       <Link to="/">
+  //         <Button marginTop="85px">Go Home</Button>
+  //       </Link>
+  //     </Layout>
+  //   )
+  // }
+
+  render() {
+    const siteTitle = "About Me here"
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <h1> About Me </h1>
+        <p> this is the stuff about me </p>
+        <Link to="/"> Return home </Link> <br />
+        <Link to="/counter"> Go to Counter </Link>
+      </Layout>
+    )
+  }
+}
+
+export default AboutMe
+
+// export const pageQuery = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+//       edges {
+//         node {
+//           excerpt
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             date(formatString: "MMMM DD, YYYY")
+//             title
+//             description
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,92 @@ import SEO from "../components/seo"
 import Button from "../components/button"
 
 import Placeholder from "../components/placeholder"
-import YoungRocky from "../../content/assets/young-rocky.jpeg"
 import "../styles/index.scss"
+
+//images
+import YoungRocky from "../../content/assets/young-rocky.jpeg"
+import Elearning from "../../content/assets/elearning.png"
+import Robot from "../../content/assets/robot.png"
+
+function Hi() {
+  return (
+    <div className="header">
+      <img src={YoungRocky} alt="A younger more stylish me" width={300} />
+      <p>
+        I&#8217;ve never been one to sit around and wait until the stars align
+        and it&#8217;s the perfect time. <br />
+        <br /> I look for opportunities. If I can&#8217;t find them, I make
+        them. Break things. Fail. Try again. Life is too short. Do stuff that
+        matters.
+        <br />
+        <br />
+        What&#8217;s not okay? Stagnation. Ideas without thinking 5 steps deep.
+        Saying that it&#8217;s &#8220;Somebody else&#8217;s job.&#8221; Doing
+        things because &#8220;That&#8217;s how we&#8217;ve always done
+        it.&#8221; <br />
+        <br /> Be whimsy. Think creatively. Build cool things.
+      </p>
+    </div>
+  )
+}
+const CardWrapper = props => {
+  return (
+    <div className="card">
+      <a href={props.link}>
+        <h3 className="card-title"> {props.title}</h3>{" "}
+      </a>
+      <a href={props.link}>
+        <img className="card-image" src={props.image} />
+      </a>
+
+      {props.body ? <p> {props.body} </p> : null}
+    </div>
+  )
+}
+
+function RecentWork() {
+  return (
+    <div>
+      <h2 className="main-title">Recent Work </h2>
+      <div className="card-container">
+        <CardWrapper title="OTHER PROJECT" image={Elearning} link="#" />
+        <CardWrapper title="OTHER PROJECT" image={Robot} link="#" />
+        <CardWrapper title="OTHER PROJECT" image={Elearning} link="#" />
+        <CardWrapper title="OTHER PROJECT" image={Robot} link="#" />
+        <CardWrapper title="OTHER PROJECT" image={Elearning} link="#" />
+        <CardWrapper title="OTHER PROJECT" image={Robot} link="#" />
+      </div>
+    </div>
+  )
+}
+
+function NewBlogPost() {
+  return (
+    <div className="blog-container">
+      <h2 className="main-title">New Blog Posts </h2>
+      <div className="card-container blog">
+        <CardWrapper
+          title="Coding"
+          image={Elearning}
+          body="Coding and How-tos guides. Education."
+          link="a"
+        />
+        <CardWrapper
+          title="Interests"
+          image={Robot}
+          body="Less techie/professional. Hacks!"
+          link="b"
+        />
+        <CardWrapper
+          title="Professional"
+          image={Elearning}
+          body="team, management, or code thoughts"
+          link="#"
+        />
+      </div>
+    </div>
+  )
+}
 
 class IndexPage extends React.Component {
   render() {
@@ -19,32 +103,11 @@ class IndexPage extends React.Component {
           title="Hey Its Rocky's Blog!"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <div className="header">
-          <img src={YoungRocky} alt="A younger more stylish me" width={300} />
-          <p>
-            I&#8217;ve never been one to sit around and wait until the stars
-            align and it&#8217;s the perfect time. <br />
-            <br /> I look for opportunities. If I can&#8217;t find them, I make
-            them. Break things. Fail. Try again. Life is too short. Do stuff
-            that matters.
-            <br />
-            <br />
-            What&#8217;s not okay? Stagnation. Ideas without thinking 5 steps
-            deep. Saying that it&#8217;s &#8220;Somebody else&#8217;s
-            job.&#8221; Doing things because &#8220;That&#8217;s how we&#8217;ve
-            always done it.&#8221; <br />
-            <br /> Be whimsy. Think creatively. Build cool things.
-          </p>
-        </div>
 
-        {/* <img src="https://i.imgur.com/rqBKk5a.jpg" /> */}
-        {/* <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" /> */}
-        <h1>
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Hi I'm Rocky.</p>
+        <Hi />
+        <RecentWork />
+        <NewBlogPost />
+        {/* 
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
@@ -53,7 +116,7 @@ class IndexPage extends React.Component {
         </Link>
         <Link to="/about-me/">
           <Button marginTop="35px">About Me</Button>
-        </Link>
+        </Link> */}
 
         {/* <Placeholder /> */}
       </Layout>
